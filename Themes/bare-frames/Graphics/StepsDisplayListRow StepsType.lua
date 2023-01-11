83bc15849afaa0d7e21053e1bdf80e8b0c00c744
karatovv <110087329+karatovv@users.noreply.github.com>
2023-01-11 22:26:29 +0400

    Delete Themes/bare-frames directory

--- Themes/bare-frames/Graphics/StepsDisplayListRow StepsType.lua
+++
@@ -1,9 +0,0 @@
--- Controls the StepsType actor for the StepsDisplayListRow
-
-local t = LoadFont("Common normal") .. {
-    InitCommand = function(self)
-        self:zoom(0.3, maxwidth, 5 / 0.3)
-    end
-}
-
-return t
